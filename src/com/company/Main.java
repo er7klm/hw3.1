55ff5403e867330b0bf86fe25c86a3b5e5eb0dda
ernisklm@gmail.com <ernisklm@gmail.com>
2020-06-14 21:42:45 +0600

    Исправление ошибок.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double numb[] = {3.2, 1.1, -7.7, 30.3, -21.6, 42.2, -8.3,
+        double[] numb = {3.2, 1.1, -7.7, 30.3, -21.6, 42.2, -8.3,
                 -12.5, 64.4, 9.9, 47.8, -20.5, -11.3, 53.7, 77.1};
         double summa = 0;
-        for (double k : numb)  {
+        double i = 0;
+        boolean otr = false;
+
+        for (double k : numb) {
             if (k < 0) {
-                continue;
+                otr = true;
+            } else {
+                if (otr) {
+                    i++;
+                    summa += k;
+                }
             }
-        summa += k;
         }
-        System.out.println(summa/numb.length);
+
+        System.out.println(summa / i);
     }
 }
