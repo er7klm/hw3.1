7006c083dff63b7420db47c4535c442108725c9d
ernisklm@gmail.com <ernisklm@gmail.com>
2020-06-14 13:55:20 +0600

    Домашнее задание 3.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double numb[] = {3.2, 1.1, -7.7, 30.3, -21.6, 42.2, -8.3,
+                -12.5, 64.4, 9.9, 47.8, -20.5, -11.3, 53.7, 77.1};
+        double summa = 0;
+        for (double k : numb)  {
+            if (k < 0) {
+                continue;
+            }
+        summa += k;
+        }
+        System.out.println(summa/numb.length);
+    }
+}
